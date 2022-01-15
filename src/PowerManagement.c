59e3dcf7a2d7996465199d8e463432bb9fbca65b
Ovidiu22 <75970114+Ovidiu22@users.noreply.github.com>
2022-01-15 09:49:10 +0100

    Function complete

--- src/PowerManagement.c
+++ src/PowerManagement.c
@@ -1,10 +1,12 @@
-
-#include <avr/interrupt.h>
-#include <avr/sleep.h>
-#include <avr/power.h>
 #include "PowerManagement.h"
 
-extern int f_wdt;
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+
+
+
 
 /* *****************************************************************
 Name:		PwrMngmnt_main()
@@ -16,32 +18,38 @@ void PwrMngmnt_main(void)
 {
 	/* Initialize watchdog */
 	PwrMngmnt_init();
-	
-	/* Start watchdog */
-	WDG_TRIG;
-	
+
 	/* Re-enter sleep mode. */
 	enterSleep();
 }
 
 /* *****************************************************************
-Name:		PwrMngmnt_ini()
+Name:		PwrMngmnt_init()
 Inputs:		none
 Outputs:	none
 Description:initialization of power management / watchdog
 ******************************************************************** */
 void PwrMngmnt_init(void)
 {
-  /* Clear the reset flag. */
-  MCUSR &= ~(1<<WDRF);
+	/* Disable global interrupts */
+	cli(); 
+	
+	/* Clear the reset flag. */
+	MCUSR &= ~(1<<WDRF); // WDRF is set if a WDG system reset occurs
   
-  /* In order to change WDE or the prescaler, we need to
-   * set WDCE (This will allow updates for 4 clock cycles).
-   */
-  WDTCSR |= (1<<WDCE) | (1<<WDE);
+	/* In order to change WDE or the prescaler, we need to
+	* set WDCE (This will allow updates for 4 clock cycles).
+	*/
+	WDTCSR |= (1<<WDCE) | (1<<WDE);
 
-  /* set new watchdog timeout prescaler value */
-  WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+	/* set new watchdog timeout prescaler value */
+	WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
+  
+	/* Enable global interrupts */
+	sei();
+	
+	/* Enable watchdog interrupt */
+	WDTCSR |= (1 << WDIE);
 }
 
 /* *****************************************************************
@@ -52,19 +60,18 @@ Description:configures the sleep mode
 ******************************************************************** */
 void enterSleep(void)
 {
-	
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+	/* Set sleep mode */
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+	/* Enable sleep */
+	sleep_enable();
   
-  /* Enable sleep */
-  sleep_enable();
+	/* Now enter sleep mode. */
+	sleep_mode();
+
+	/* The program will continue from here after the WDT timeout*/
+	sleep_disable(); /* First thing to do is disable sleep. */
   
-  /* Now enter sleep mode. */
-  sleep_mode();
-  
-  /* The program will continue from here after the WDT timeout*/
-  sleep_disable(); /* First thing to do is disable sleep. */
-  
-  /* Re-enable the peripherals. */
-  power_all_enable();
-  
+	/* Re-enable the peripherals. */
+	power_all_enable();
 }
--- src/main.c
+++ src/main.c
@@ -1,29 +1,56 @@
-
+/*******************************/
+/*** Created by Ovidiu Sabau ***/
+/***	15th January 2022	 ***/
+/*******************************/
 
 #include "Defines.h"
 #include "PowerManagement.h"
 
 #include <avr/interrupt.h>
-#include <avr/wdt.h>
-#include <avr/io.h>
 #include <util/delay.h>
+#include <avr/wdt.h>
 
-
+volatile int f_wdt = 1;
 
 
 int main(void)
 {
 	LED_DDR |= 1<<LED_pin;
 	
-	while(1)
+	while( f_wdt == 1)
 	{
+		/* Some sequence */
 		LED_port |= (1<<LED_pin);	// Turn LED on
-		_delay_ms(1000);			// Wait 1 sec
+		_delay_ms(3000);			// Wait 1 sec
 		LED_port &= ~(1<<LED_pin);	// Turn LED off
-		_delay_ms(1000);			// Wait 1 sec
+		_delay_ms(3000);			// Wait 3 sec to stabilize current -> reference for sleep modes
+	
+		/* Clear the watchdog flag */
+		f_wdt = 0;
+		
+		/* Power management */
+		PwrMngmnt_main();
 	}
-	
-	PwrMngmnt_main();
-	
 	return 0;
+}
+
+/***************************************************
+ *  Name:        ISR(WDT_vect)
+ *
+ *  Returns:     Nothing.
+ *
+ *  Parameters:  None.
+ *
+ *  Description: Watchdog Interrupt Service. This
+ *               is executed when watchdog timed out.
+ *
+ ***************************************************/
+ISR(WDT_vect)
+{
+	LED_port |= (1<<LED_pin);
+	if(f_wdt == 0)
+	{
+		f_wdt = 1;
+	}
+
 }
